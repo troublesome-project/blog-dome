4a072452f2b8af9de8ad72b286a90563ff483316
chengkw <chengkw@zgtfintech.com>
2020-03-01 16:26:08 +0800

    前端模块开发提交

--- src/views/class/index.vue
+++ src/views/class/index.vue
@@ -1,5 +1,39 @@
 <template>
     <div>
-        课程
+        <Header/>
+        <div class="leftBox">
+            <LeftView :Height="leftHeight" />
+        </div>
+        
+        <div>
+            <!-- <RightView /> -->
+        </div>
     </div>
 </template>
+<script>
+import LeftView from '@/components/leftView/index'
+import RightView from '@/components/rightView/index'
+import { heightGet } from '@/uitls/domList'
+export default {
+    name: 'class',
+    components: {
+        LeftView,
+        RightView
+    },
+    data() {
+        return {
+            leftHeight: 0,
+            rightWidth: 0
+        }
+    },
+    mounted() {
+        this.leftHeight = heightGet()
+    }
+}
+</script>
+<style lang="scss" scoped>
+.leftBox{
+    width: auto;
+    height: auto;
+}
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <!-- <div class="header_box">CKW、的资料卡</div> -->
+        <Header/>
+        <div class="modular_box">
+            <el-row>
+                <el-col :span='12' v-for="item in domeList" :key="item.id">
+                    <div @click="change(item.routers)" :class="'modular_box_list modular_box_list' + item.type">{{item.name}}</div>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            domeList: [
+                { name: '前端资料', routers: '/class', type: 'one', id: '0' },
+                { name: '名言警句', routers: '/', type: 'two', id: '1' },
+                { name: '书籍推荐', routers: '/', type: 'three', id: '2' },
+                { name: '歌曲收藏', routers: '/', type: 'four', id: '3' }
+            ]
+        }
+    },
+    methods: {
+        change(row) {
+            this.$router.push({
+                path: row
+            })
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.modular_box{
+    padding: 0.2rem;
+    .modular_box_list{
+        margin: 0.2rem;
+        height: 2.5rem;
+        background-color: #f60;
+        font-size: 0.3rem;
+        text-align: center;
+        line-height: 2.5rem;
+        font-weight: 400;
+        box-shadow: 0 0.05rem 0.05rem;
+        color: #e6e6e6;
+    }
+    .modular_box_listone{
+        background-color: #909399;
+    }
+    .modular_box_listtwo{
+        background-color: #67C23A;
+    }
+    .modular_box_listthree{
+        background-color: #E6A23C;
+    }
+    .modular_box_listfour{
+        background-color: #F56C6C;
+    }
+}
+</style>
